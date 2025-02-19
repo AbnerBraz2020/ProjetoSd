119856fd6602ca57536ae75986d8fd2c66b44c44
Abner Gomes <abnerbraz2020@gmail.com>
2025-02-19 09:09:11 -0300

    QuebraCabeças

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function criarPecas() {
 
 function embaralharArray(array) {
     for (let i =array.length - 1; i > 0; i--) {
-        const j =Math.floor(Math.random() * (i + i));
+        const j =Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
